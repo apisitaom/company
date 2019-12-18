c47719c7e40efaf3285e6f5b4552ca249dfde89e
apisitprompha <apisitprompha@gmail.com>
2019-12-18 17:28:09 +0000

    สร้าง FORM EMPLYEE LISTS

--- frontend/src/views/Pages/Employee/Lists/Lists.js
+++ frontend/src/views/Pages/Employee/Lists/Lists.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react'
-
+import { Row, Col } from 'reactstrap'
+import { Table } from 'antd'
 export default class Lists extends Component {
     render() {
+        const columns = [
+            {
+              title: 'Name',
+              dataIndex: 'name',
+              key: 'name',
+            },
+            {
+              title: 'Age',
+              dataIndex: 'age',
+              key: 'age',
+            },
+            {
+              title: 'Address',
+              dataIndex: 'address',
+              key: 'address',
+            },
+            {
+              title: 'Tags',
+              key: 'tags',
+              dataIndex: 'tags',
+            },
+            {
+              title: 'Action',
+              key: 'action',
+            },
+          ];
+
+        const data = [
+            {
+              key: '1',
+              name: 'John Brown',
+              age: 32,
+              address: 'New York No. 1 Lake Park',
+              tags: ['nice', 'developer'],
+            },
+            {
+              key: '2',
+              name: 'Jim Green',
+              age: 42,
+              address: 'London No. 1 Lake Park',
+              tags: ['loser'],
+            },
+            {
+              key: '3',
+              name: 'Joe Black',
+              age: 32,
+              address: 'Sidney No. 1 Lake Park',
+              tags: ['cool', 'teacher'],
+            },
+          ];
         return (
             <div>
-                <h1>Employee Lists</h1>
+                <Row>
+                    <Col>
+                    <Table 
+                    columns={columns} 
+                    dataSource={data} />
+                    </Col>
+                </Row>
             </div>
         )
     }
