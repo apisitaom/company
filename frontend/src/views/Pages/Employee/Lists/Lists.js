583b1a1e60607d6effdfc39a7bacb3464bbd2716
apisitprompha <apisitprompha@gmail.com>
2019-12-19 14:56:48 +0000

    เพิ่ม FORM STRE LISTS

--- frontend/src/views/Pages/Employee/Lists/Lists.js
+++ frontend/src/views/Pages/Employee/Lists/Lists.js
@@ -5,61 +5,60 @@ export default class Lists extends Component {
     render() {
         const columns = [
             {
-              title: 'Name',
-              dataIndex: 'name',
-              key: 'name',
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
             },
             {
-              title: 'Age',
-              dataIndex: 'age',
-              key: 'age',
+                title: 'Age',
+                dataIndex: 'age',
+                key: 'age',
             },
             {
-              title: 'Address',
-              dataIndex: 'address',
-              key: 'address',
+                title: 'Address',
+                dataIndex: 'address',
+                key: 'address',
             },
             {
-              title: 'Tags',
-              key: 'tags',
-              dataIndex: 'tags',
+                title: 'Tags',
+                key: 'tags',
+                dataIndex: 'tags',
             },
             {
-              title: 'Action',
-              key: 'action',
+                title: 'Action',
+                key: 'action',
             },
-          ];
-
+        ];
         const data = [
             {
-              key: '1',
-              name: 'John Brown',
-              age: 32,
-              address: 'New York No. 1 Lake Park',
-              tags: ['nice', 'developer'],
+                key: '1',
+                name: 'John Brown',
+                age: 32,
+                address: 'New York No. 1 Lake Park',
+                tags: ['nice', 'developer'],
             },
             {
-              key: '2',
-              name: 'Jim Green',
-              age: 42,
-              address: 'London No. 1 Lake Park',
-              tags: ['loser'],
+                key: '2',
+                name: 'Jim Green',
+                age: 42,
+                address: 'London No. 1 Lake Park',
+                tags: ['loser'],
             },
             {
-              key: '3',
-              name: 'Joe Black',
-              age: 32,
-              address: 'Sidney No. 1 Lake Park',
-              tags: ['cool', 'teacher'],
+                key: '3',
+                name: 'Joe Black',
+                age: 32,
+                address: 'Sidney No. 1 Lake Park',
+                tags: ['cool', 'teacher'],
             },
-          ];
+        ];
         return (
             <div>
                 <Row>
                     <Col>
-                    <Table 
-                    columns={columns} 
-                    dataSource={data} />
+                        <Table
+                            columns={columns}
+                            dataSource={data} />
                     </Col>
                 </Row>
             </div>
