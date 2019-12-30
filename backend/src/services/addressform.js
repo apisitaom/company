21ba3535f737cea9abf74f83b1a7ad2da751e1bd
apisitprompha <apisitprompha@gmail.com>
2019-12-30 05:24:11 +0000

    ADD STRUCTURE services

--- backend/src/services/addressform.js
+++ backend/src/services/addressform.js
@@ -1,4 +1,37 @@
 const Addressform = require('../models/addressform');
-const responce = require('../lib/responces');
+const responces = require('../lib/responces');
 const success = require('../lib/success');
 const errors = require('../lib/error');
+
+
+async function add (req, res, next) {
+    try {
+        return responces.success(res, success.success)
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
+
+async function edit (req, res, next) {
+    try {
+        return responces.success(res, success.success)
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
+
+async function lists (req, res, next) {
+    try {
+        return responces.success(res, success.success)
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
+
+async function deletes (req, res, next) {
+    try {
+        return responces.success(res, success.success)
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
---
+++ backend/src/services/store.js
@@ -0,0 +1,37 @@
+const Store = require('../models/store');
+const responces = require('../lib/responces');
+const success = require('../lib/success');
+const errors = require('../lib/error');
+
+
+async function add (req, res, next) {
+    try {
+        return responces.success(res, success.success)
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
+
+async function edit (req, res, next) {
+    try {
+        return responces.success(res, success.success)
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
+
+async function lists (req, res, next) {
+    try {
+        return responces.success(res, success.success)
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
+
+async function deletes (req, res, next) {
+    try {
+        return responces.success(res, success.success)
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
