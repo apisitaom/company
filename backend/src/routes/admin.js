139ab9a7b8ec7b04044db11dc9752d42f8117634
apisitprompha <apisitprompha@gmail.com>
2020-01-19 17:17:00 +0700

    admin/services ADD login function

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -5,6 +5,7 @@ router.get('/', (req, res) => {res.json({info : `admin route`})});
 router.get('/lists', admin.lists);
 
 router.post('/add', admin.add);
+router.post('/login', admin.login);
 router.put('/edit/:id', admin.edit);
 
 router.delete('/delete/:id', admin.deletes);
--- backend/src/services/admin.js
+++ backend/src/services/admin.js
@@ -2,6 +2,7 @@ const Admin = require('../models/admin');
 const responces = require('../lib/responces');
 const success = require('../lib/success');
 const errors = require('../lib/error');
+const middle = require('../lib/middle');
 
 function add (req, res, next) {
     const { email, password } = req.body
@@ -47,4 +48,19 @@ async function deletes (req, res, next) {
     }
 }
 
-module.exports = { add, edit, lists, deletes }
+async function login (req, res, next) {
+    const { email, password } = req.body
+    try {
+        // select email, password where email = $1, and password = $2;
+        const rows = await Admin.find({email: `${email}`, password: `${password}`}, {password, email});
+        if( rows[0] ) {
+            const token = middle.generateToken(rows[0]._id);
+            return responces.token(res, success.success, '', token);
+        }
+        return responces.error(res, errors.params);
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
+
+module.exports = { add, edit, lists, deletes, login }
